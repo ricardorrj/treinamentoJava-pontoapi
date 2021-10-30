2fda12e89a5a0efd665095de3c4b9899aad307a8
Ricardo Jr <ricardorrj25@gmail.com>
2021-10-30 15:18:56 -0300

    Start Project

---
+++ src/test/java/com/ricardorrj/pontoeletronico/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardorrj.pontoeletronico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
